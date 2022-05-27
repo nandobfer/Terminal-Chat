21dd360dbef0d1bd51a318641164d91a5a648da0
Burgos <nandobfer@yahoo.com.br>
2022-05-27 11:30:37 -0300

    added pip install reqcheq to install

--- install.py
+++ install.py
@@ -1,7 +1,11 @@
 # script to be run the first time before the chat program
 # it will install required modules and add local dir to git safe directory list
 
-import os, reqcheq, pathlib
+import os, pathlib
+
+os.system('pip install reqcheq')
+
+import reqcheq
 
 path = pathlib.Path().resolve()
 git_config = f'git config --global --add safe.directory {path}'
